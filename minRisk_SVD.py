46acfac90297e6b3be322443ffb039865588a282
cains <l6751902@163.com>
2019-06-21 18:41:33 +0800

    add SVD filter

--- minRisk_SVD.py
+++ minRisk_SVD.py
@@ -1,5 +1,9 @@
 # coding=utf-8
 
+# https://srome.github.io/Eigenvesting-III-Random-Matrix-Filtering-In-Finance/
+# 不过文章里分布函数写错了。
+# 最终换成这篇的。http://www.doc88.com/p-9877831718045.html
+
 import matplotlib
 import QUANTAXIS as QA
 import pandas as pd
@@ -12,16 +16,16 @@ import matplotlib.pyplot as plt
 # result = pd.merge(stock_diff,infos.loc[codes]['name'], left_index=True,right_index=True)
 
 codes = QA.QA_fetch_stock_block_adv().get_block(['生物医药','化学制药']).code
-data =QA.QA_fetch_stock_day_adv(codes[:10],'2017-01-05','2017-03-25').to_hfq()
+data =QA.QA_fetch_stock_day_adv(codes,'2017-01-05','2017-12-25').to_hfq()
 
-data =QA.QA_fetch_stock_day_adv(['002415','601155','000735','300558'],'2017-01-05','2017-12-25').to_hfq()
+# data =QA.QA_fetch_stock_day_adv(['002415','601155','000735','300558'],'2017-01-05','2017-12-25').to_hfq()
 
-data.price_chg.values
-data.price_chg[:, '000735'].values
-data.price_chg['2017-01-10']
-data.data.loc["2017-01-03","000735"]
-
-data.price_chg.unstack().values.T
+# data.price_chg.values
+# data.price_chg[:, '000735'].values
+# data.price_chg['2017-01-10']
+# data.data.loc["2017-01-03","000735"]
+#
+# data.price_chg.unstack().values.T
 
 ###################
 # lgR = np.log(data.close/data.pre_close)
@@ -47,10 +51,12 @@ data.price_chg.unstack().values.T
 # pd.DataFrame(lowDDataMat).plot(x=0,y=1,kind='scatter')
 
 ###################
+samples = data.close.unstack()
+# samples.to_excel('1.xls')
+samples.dropna(0, inplace= True)
 lgR = np.log(data.close/data.pre_close)
-lgR_us = lgR.unstack()
-lgR_us.dropna(0, inplace= True)
-lgR_mat = lgR_us
+lgR.dropna(0, inplace= True)
+lgR_mat = lgR
 
 # ret = data.pct_change
 # lgR = ret.apply(lambda x : np.log(x+1))
@@ -62,39 +68,16 @@ lgR_mat = lgR_us
 corMat = np.mat(lgR_mat.interpolate().corr())
 eigVals,eigVects=np.linalg.eig(corMat) #求特征值和特征向量,特征向量是按列放的，即一列代表一个特征向量
 
-lowDDataMat=corMat*n_eigVect               #低维特征空间的数据
-reconMat=(lowDDataMat*n_eigVect.T)  #重构数据
-pd.DataFrame(lowDDataMat).plot(x=0,y=1,kind='scatter')
 
 
 # Definition of the Marchenko-Pastur density
-#http://www.doc88.com/p-9877831718045.html
 def marchenko_pastur_pdf(x, lanbda, sigma=1):
     b=np.power(sigma*sigma*(1 + np.sqrt(lanbda)),2) # Largest eigenvalue
     a=np.power(sigma*sigma*(1 - np.sqrt(lanbda)),2)# Smallest eigenvalue
     return (1/(2*np.pi*sigma*sigma*lanbda*x))*np.sqrt((b-x)*(x-a))*(0 if (x > b or x <a ) else 1)
 
-N,T = lgR_mat.shape
-lanbda = T/N
-sigma = 1
-
-max_theoretical_eval = np.power(sigma*sigma*(1 + np.sqrt(lanbda)),2)
-
-# Filter the eigenvalues out
-eigVals[eigVals <= max_theoretical_eval] = 0
-
-filtered_matrix = np.dot(eigVects, np.dot(np.diag(eigVals), eigVects.T))
-
-
-f = plt.figure()
-ax = plt.subplot(121)
-ax.imshow(corMat)
-plt.title("Original")
-ax = plt.subplot(122)
-plt.title("Filtered")
-ax.imshow(filtered_matrix)
-a = ax.imshow(filtered_matrix)
-cbar = f.colorbar(a, ticks=[-1, 0, 1])
+def marchenko_pastur_supremum(lanbda, sigma=1):
+    return np.power(sigma*sigma*(1 + np.sqrt(lanbda)),2)
 
 
 def show_compare_plot(eigVals, lanbda, sigma):
@@ -108,3 +91,82 @@ def show_compare_plot(eigVals, lanbda, sigma):
     ax.plot(x,f(x), linewidth=4, color = 'r')
 
     plt.show()
+
+N,T = lgR_mat.shape
+lanbda = T/N
+sigma = 1
+
+###
+show_compare_plot(eigVals, lanbda, sigma)
+
+max_theoretical_eval = marchenko_pastur_supremum(lanbda, sigma=1)
+
+# Filter the eigenvalues out
+eigVals[eigVals <= max_theoretical_eval] = 0
+
+filtered_matrix = np.dot(eigVects, np.dot(np.diag(eigVals), eigVects.T))
+
+# 显示过滤前相关性矩阵图和过滤后图
+f = plt.figure()
+ax = plt.subplot(121)
+ax.imshow(corMat)
+plt.title("Original")
+ax = plt.subplot(122)
+plt.title("Filtered")
+ax.imshow(filtered_matrix)
+a = ax.imshow(filtered_matrix)
+cbar = f.colorbar(a, ticks=[-1, 0, 1])
+
+
+
+covariance_matrix = np.cov(samples, rowvar=0)
+inv_cov_mat = np.linalg.pinv(covariance_matrix)
+
+## Construct minimum variance weights
+##  min  w∑w  s.t ∑w = 1 （第一个sigma是协方差，第二个是求和）
+## 最小化资产组合风险的凸优化问题，解得权重
+##  ∑^(-1)*1  /  (∑^(-1) * 1 ) * 1
+ones = np.ones(len(inv_cov_mat))
+inv_dot_ones = np.dot(inv_cov_mat, ones)
+min_var_weights = inv_dot_ones/ np.dot( inv_dot_ones , ones)
+
+# 求标准差
+variances = np.diag(np.cov(lgR_mat,rowvar=0))
+standard_deviations = np.sqrt(variances)
+
+##  ∑ 展开可以写成  ∑∑ ρ_ij * σ_i * σ_j
+## 所以可以用上面算好的相关系数矩阵，求解新的协方差矩阵。
+## ∑` = √(∑*I) Ρ √(∑*I)
+filtered_cov = np.dot(np.diag(standard_deviations),
+np.dot(filtered_matrix,np.diag(standard_deviations)))
+
+## Construct minimum variance weights
+##  ∑^(-1)*1   /  (∑^(-1) * 1 ) * 1
+##  顺手做个np.asarray转换，防止后面matrix没法画图
+filt_inv_cov = np.linalg.pinv(np.asarray(filtered_cov))
+# ones = np.ones(len(filt_inv_cov))
+inv_dot_ones = np.dot(filt_inv_cov, ones)
+filt_min_var_weights = inv_dot_ones/ np.dot( inv_dot_ones , ones)
+
+
+# 显示过滤前权重图和过滤后图
+plt.figure(figsize=(16,4))
+ax = plt.subplot(121)
+min_var_portfolio = pd.DataFrame(data= min_var_weights,
+                                 columns = ['Investment Weight'],
+                                 index = samples.columns.values.tolist())
+min_var_portfolio.plot(kind = 'bar', ax = ax)
+plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
+plt.title('Minimum Variance')
+
+ax = plt.subplot(122)
+filt_min_var_portfolio = pd.DataFrame(data= filt_min_var_weights,
+                                 columns = ['Investment Weight'],
+                                 index = samples.columns.values.tolist())
+filt_min_var_portfolio.plot(kind = 'bar', ax = ax)
+plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
+plt.title('Filtered Minimum Variance')
+
+
+
+
